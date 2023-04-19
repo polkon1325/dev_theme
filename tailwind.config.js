3b01c63e54f47b82ba53f5db17722776109f2a60
Vryan Luis Gahoy <vryanluis@gmail.com>
2023-04-19 16:44:09 +0800

    Added Custom colors in Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,21 @@ module.exports = {
     './config/customers/*.liquid',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'bright-orange': '#FE5200'
+      },
+    },
   },
   plugins: [],
 }
